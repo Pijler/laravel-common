e7dcdc47fb56a6485ccbc48d235611b59363056b
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-16 18:57:05 -0300

    feat(channel): creating storage channel

---
+++ src/Channel/StorageChannel.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Common\Channel;
+
+use Closure;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
+
+class StorageChannel
+{
+    /**
+     * The message instance.
+     */
+    private ?MailMessage $message = null;
+
+    /**
+     * The callback that should be used to get storage path.
+     */
+    public static ?Closure $storagePathCallback;
+
+    /**
+     * Send the given notification.
+     */
+    public function send($notifiable, Notification $notification): void
+    {
+        $this->message = $notification->toMail($notifiable);
+
+        $this->saveInDatabase($notifiable, $notification);
+
+        Storage::put($this->getPath($notification), $this->message->render());
+    }
+
+    /**
+     * Send the given notification.
+     */
+    protected function saveInDatabase($notifiable, Notification $notification): mixed
+    {
+        return $notifiable->routeNotificationFor('database', $notification)->updateOrCreate([
+            'id' => $notification->id,
+        ], $this->buildPayload($notification));
+    }
+
+    /**
+     * Build an array payload for the DatabaseNotification Model.
+     */
+    protected function buildPayload(Notification $notification): array
+    {
+        return [
+            'type' => get_class($notification),
+            'data' => [
+                'subject' => $this->message->subject,
+                'path' => $this->getPath($notification),
+            ],
+        ];
+    }
+
+    /**
+     * Get the path for the notification.
+     */
+    protected function getPath(Notification $notification): string
+    {
+        if (static::$storagePathCallback) {
+            return call_user_func(static::$storagePathCallback, $notification);
+        }
+
+        $date = date('Y-m-d');
+
+        $environment = App::environment();
+
+        return "/notifications/{$environment}/{$date}/{$notification->id}.html";
+    }
+}
