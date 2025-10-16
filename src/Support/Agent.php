f81c124abe1a6e6cbb49839255c427af80e29011
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-16 19:01:13 -0300

    feat(agent): adding agent class

---
+++ src/Support/Agent.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Common\Support;
+
+use Closure;
+use Detection\MobileDetect;
+
+/**
+ * @copyright https://github.com/laravel/jetstream/blob/4.x/src/Agent.php
+ * @copyright https://github.com/jenssegers/agent/blob/master/src/Agent.php
+ */
+class Agent extends MobileDetect
+{
+    /**
+     * List of additional operating systems.
+     */
+    protected static array $additionalOperatingSystems = [
+        'Windows' => 'Windows',
+        'Windows NT' => 'Windows NT',
+        'OS X' => 'Mac OS X',
+        'Debian' => 'Debian',
+        'Ubuntu' => 'Ubuntu',
+        'Macintosh' => 'PPC',
+        'OpenBSD' => 'OpenBSD',
+        'Linux' => 'Linux',
+        'ChromeOS' => 'CrOS',
+    ];
+
+    /**
+     * List of additional browsers.
+     */
+    protected static array $additionalBrowsers = [
+        'Opera Mini' => 'Opera Mini',
+        'Opera' => 'Opera|OPR',
+        'Edge' => 'Edge|Edg',
+        'Coc Coc' => 'coc_coc_browser',
+        'UCBrowser' => 'UCBrowser',
+        'Vivaldi' => 'Vivaldi',
+        'Chrome' => 'Chrome',
+        'Firefox' => 'Firefox',
+        'Safari' => 'Safari',
+        'IE' => 'MSIE|IEMobile|MSIEMobile|Trident/[.0-9]+',
+        'Netscape' => 'Netscape',
+        'Mozilla' => 'Mozilla',
+        'WeChat' => 'MicroMessenger',
+    ];
+
+    /**
+     * Key value store for resolved strings.
+     */
+    protected array $store = [];
+
+    /**
+     * Get the platform name from the User Agent.
+     */
+    public function platform(): ?string
+    {
+        return $this->retrieveUsingCacheOrResolve('agent.platform', function () {
+            return $this->findDetectionRulesAgainstUserAgent(
+                $this->mergeRules(MobileDetect::getOperatingSystems(), static::$additionalOperatingSystems)
+            );
+        });
+    }
+
+    /**
+     * Get the browser name from the User Agent.
+     */
+    public function browser(): ?string
+    {
+        return $this->retrieveUsingCacheOrResolve('agent.browser', function () {
+            return $this->findDetectionRulesAgainstUserAgent(
+                $this->mergeRules(static::$additionalBrowsers, MobileDetect::getBrowsers())
+            );
+        });
+    }
+
+    /**
+     * Determine if the device is a desktop computer.
+     */
+    public function isDesktop(): bool
+    {
+        return $this->retrieveUsingCacheOrResolve('agent.desktop', function () {
+            // Check specifically for cloudfront headers if the useragent === 'Amazon CloudFront'
+            if ($this->getUserAgent() === static::$cloudFrontUA && $this->getHttpHeader('HTTP_CLOUDFRONT_IS_DESKTOP_VIEWER') === 'true') {
+                return true;
+            }
+
+            return ! $this->isMobile() && ! $this->isTablet();
+        });
+    }
+
+    /**
+     * Match a detection rule and return the matched key.
+     */
+    protected function findDetectionRulesAgainstUserAgent(array $rules): ?string
+    {
+        $userAgent = $this->getUserAgent();
+
+        foreach ($rules as $key => $regex) {
+            if (empty($regex)) {
+                continue;
+            }
+
+            if ($this->match($regex, $userAgent)) {
+                return $key ?: reset($this->matchesArray);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Retrieve from the given key from the cache or resolve the value.
+     */
+    protected function retrieveUsingCacheOrResolve(string $key, Closure $callback): mixed
+    {
+        $cacheKey = $this->createCacheKey($key);
+
+        if (! is_null($cacheItem = $this->store[$cacheKey] ?? null)) {
+            return $cacheItem;
+        }
+
+        return tap(call_user_func($callback), function ($result) use ($cacheKey) {
+            $this->store[$cacheKey] = $result;
+        });
+    }
+
+    /**
+     * Merge multiple rules into one array.
+     */
+    protected function mergeRules(...$all): array
+    {
+        $merged = [];
+
+        foreach ($all as $rules) {
+            foreach ($rules as $key => $value) {
+                if (empty($merged[$key])) {
+                    $merged[$key] = $value;
+                } elseif (is_array($merged[$key])) {
+                    $merged[$key][] = $value;
+                } else {
+                    $merged[$key] .= '|'.$value;
+                }
+            }
+        }
+
+        return $merged;
+    }
+}
