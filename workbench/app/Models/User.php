7a499c0de38292b3c39374f4a0ec75e822a11ae5
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-20 15:37:38 -0300

    feat(testbench): adding laravel skeleton structure

---
+++ workbench/app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Workbench\App\Models;
+
+use Common\Traits\HasTwoFactor;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+    use HasTwoFactor;
+
+    /**
+     * The attributes that aren't mass assignable.
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+            'email_verified_at' => 'datetime',
+            'two_factor_confirmed_at' => 'datetime',
+        ];
+    }
+}
---
+++ workbench/database/factories/UserFactory.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Workbench\Database\Factories;
+
+use Common\Support\TwoFactor\RecoveryCode;
+use Common\Support\TwoFactor\TwoFactorAuthentication;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Workbench\App\Models\User;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => Carbon::now(),
+            'password' => $this->faker->password(),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn () => [
+            'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the model has two factor.
+     */
+    public function twoFactor(): static
+    {
+        return $this->secret()->confirmed()->recoveryCodes();
+    }
+
+    /**
+     * Indicate that the model has two factor secret.
+     */
+    public function secret(): static
+    {
+        return $this->state(fn () => [
+            'two_factor_secret' => app(TwoFactorAuthentication::class)->generateSecretKey(),
+        ]);
+    }
+
+    /**
+     * Indicate that the model has two factor confirmed.
+     */
+    public function confirmed(): static
+    {
+        return $this->state(fn () => [
+            'two_factor_confirmed_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * Indicate that the model has two factor recovery codes.
+     */
+    public function recoveryCodes(): static
+    {
+        return $this->state(fn () => [
+            'two_factor_recovery_codes' => RecoveryCode::generateMany(),
+        ]);
+    }
+}
